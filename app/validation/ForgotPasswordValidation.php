6c60d16925e009ef5573795092fc694070eca2d7
Stanislav Dimitrov <stan@suera.com>
2023-03-02 17:00:19 +0200

    Add ForgotPasswordValidation.php

---
+++ app/validation/ForgotPasswordValidation.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Validation;
+
+use App\Models\Users;
+use Phalcon\Filter\Validation;
+use Phalcon\Filter\Validation\Validator\Email;
+use Phalcon\Filter\Validation\Validator\PresenceOf;
+use Phalcon\Filter\Validation\Validator\Regex;
+use Phalcon\Filter\Validation\Validator\StringLength;
+use Phalcon\Filter\Validation\Validator\Uniqueness;
+
+class ForgotPasswordValidation extends Validation
+{
+    public function initialize()
+    {
+        $this->rules(
+            'email',
+            [
+                new PresenceOf([
+                    'message' => 'Email is required.',
+                    'cancelOnFail' => true
+                ]),
+                new Email([
+                    'message' => 'Enter a valid email.',
+                    'cancelOnFail' => true
+                ]),
+                new Uniqueness([
+                    'model' => new Users(),
+                    'message' => 'Email address is already in use.'
+                ])
+            ]
+        );
+
+
+    }
+
+}
