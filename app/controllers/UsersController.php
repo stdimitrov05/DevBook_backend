206b1c37305334825cc84a8f9694ad9e9c76a935
Stanislav Dimitrov <stan@suera.com>
2023-03-09 17:32:43 +0200

    Add new functions * Modify Elastic.php for update user detail + Update user details

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -9,6 +9,7 @@ use App\Exceptions\HttpExceptions\Http500Exception;
 use App\Exceptions\ServiceException;
 use App\Services\AbstractService;
 use App\Validation\BillingValidation;
+use App\Validation\EditUserDetailsValidation;
 use App\Validation\SignupValidation;
 
 /**
@@ -99,6 +100,46 @@ class UsersController extends AbstractController
                 => new Http422Exception($e->getMessage(), $e->getCode(), $e),
                 AbstractService::ERROR_IS_NOT_FOUND,
                 AbstractService::ERROR_BAD_TOKEN,
+                => new Http404Exception($e->getMessage(), $e->getCode(), $e),
+
+                default => new Http500Exception('Internal Server Error', $e->getCode(), $e),
+            };
+        }
+
+        return $response;
+    }
+
+    /**
+     * updateDetailsAction
+     * @param int $userId
+     * @retrun  null
+     * */
+    public function updateDetailsAction(int $userId)
+    {
+        $data = [];
+
+        // Collect and trim request params
+        foreach ($this->request->getPost() as $key => $value) {
+            $data[$key] = $this->request->getPost($key, ['string', 'trim']);
+        }
+
+        // Start validation
+        $validation = new EditUserDetailsValidation();
+        $messages = $validation->validate($data);
+
+        if (count($messages)) {
+            $this->throwValidationErrors($messages);
+        }
+
+        try {
+            $response = $this->usersService->updateDetails($userId, $data);
+
+        } catch (ServiceException $e) {
+            throw match ($e->getCode()) {
+                AbstractService::ERROR_USER_NOT_AUTHORIZED,
+                => new Http422Exception($e->getMessage(), $e->getCode(), $e),
+                AbstractService::ERROR_BAD_TOKEN,
+                AbstractService::ERROR_IS_NOT_FOUND,
                 => new Http404Exception($e->getMessage(), $e->getCode(), $e),
 
                 default => new Http500Exception('Internal Server Error', $e->getCode(), $e),
